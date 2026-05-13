26653f15eaa88c4d88a3f3ebabd57a94b23eb6f2
parasbhattrai <bhattraiparas17@gmail.com>
2026-05-13 11:12:59 +0530

    Add files via upload

---
+++ l4.py
@@ -0,0 +1,8 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = gcd(num1, num2)
+print("GCD =", result)
